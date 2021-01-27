b7cc6a201bf9da7623dfc9bf1c3b21bb796ff452
StarfoxMulder <cjprestia@gmail.com>
2021-01-27 13:34:58 -0500

    working createBoard method generating a grid of x width by y height

--- src/Board.js
+++ src/Board.js
@@ -30,11 +30,19 @@ import './Board.css';
  **/
 
 class Board extends Component {
-
+  static defaultProps = {
+    nrows: 5,
+    ncols: 5,
+    chanceLightStartsOn: 0.25
+  }
   constructor(props) {
     super(props);
 
     // TODO: set initial state
+    this.state = {
+      hasWon: false,
+      board: this.createBoard()
+    }
   }
 
   /** create a board nrows high/ncols wide, each cell randomly lit or unlit */
@@ -42,6 +50,15 @@ class Board extends Component {
   createBoard() {
     let board = [];
     // TODO: create array-of-arrays of true/false values
+    for(let y=0; y < this.props.nrows; y++){
+      let row = [];
+      for(let x=0; x < this.props.ncols; x++) {
+        // This pushes true or false into the array ncols number of times
+        // We are going to push rows nrows number of times determined in the parent loop
+        row.push(Math.random() < this.props.chanceLightStartsOn)
+      }
+      board.push(row)
+    }
     return board
   }
 
@@ -62,11 +79,12 @@ class Board extends Component {
     }
 
     // TODO: flip this cell and the cells around it
+    //flipCell(self) and again for each of the 4 neighbors
 
     // win when every cell is turned off
     // TODO: determine is the game has been won
 
-    this.setState({board, hasWon});
+    //this.setState({board, hasWon});
   }
 
 
@@ -79,6 +97,9 @@ class Board extends Component {
     // TODO
 
     // make table board
+    return (
+      <h1>BOARD</h1>
+    )
 
     // TODO
   }
@@ -86,3 +107,13 @@ class Board extends Component {
 
 
 export default Board;
+
+/* <table>
+<tbody>
+  <tr>
+    <Cell isLit={true} />
+    <Cell isLit={false} />
+    <Cell isLit={true} />
+  </tr>
+</tbody>
+</table> */
